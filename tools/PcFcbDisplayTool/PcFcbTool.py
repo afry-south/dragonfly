3129bd10c6c30f837e5520dcc9648a310fdfcab4
DanielStenis <daniel.stenberg@afconsult.com>
2016-04-16 14:29:29 +0200

    Modified to allow com port as string and minor fixes. Also updated Readme.

--- tools/PcFcbDisplayTool/PcFcbTool.py
+++ tools/PcFcbDisplayTool/PcFcbTool.py
@@ -4,6 +4,7 @@ import sys
 import os
 import argparse
 import time
+import math
 import serial
 import threading
 from serial.tools.list_ports import comports
@@ -15,7 +16,7 @@ from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
 import pyqtgraph as pg
 
-debug_print = False
+debug_print = True
 script_name = os.path.basename(__file__)[:-3]
 
 def pprint(format, *args):
@@ -32,11 +33,11 @@ tool_description="Sample the estimated RPY angles from the FCB card. Also see RE
 arg_parser = argparse.ArgumentParser(description=tool_description)
 arg_parser.add_argument("interval_ms", type=int, help="[100 ...] nbr of milliseconds between each state sample")
 arg_parser.add_argument("duration_s", type=int, help="[2 ..] state will be sampled for a duration of seconds")
-arg_parser.add_argument("--com", type=int, default=8, help="give the number of the COM port for STM32 Virtual Com Port (default 8)")
+arg_parser.add_argument("--com", type=str, default="dev/ttyACM0", help="give the number of the COM port for STM32 Virtual Com Port")
 cli_args = arg_parser.parse_args()
 
 pprint("starting ...")
-dprint("given args: interval_ms:%d duration_s:%d com:%d" 
+dprint("given args: interval_ms:%d duration_s:%d com:%s" 
        % (cli_args.interval_ms, cli_args.duration_s, cli_args.com))
 dprint("imports successful, Captain!")
 
@@ -48,13 +49,15 @@ roll_data = deque([float(0)]*100) # append & pop from opposite ends is thread sa
 pitch_data = deque([float(0)]*100) # append & pop from opposite ends is thread safe according to .py doc 
 yaw_data = deque([float(0)]*100) # append & pop from opposite ends is thread safe according to .py doc 
 
+RAD_TO_DEG_FACTOR = 180/math.pi
+
 def update_plot_data(new_roll_data, new_pitch_data, new_yaw_data):
     sema.acquire(blocking = 1)
-    roll_data.appendleft(new_roll_data)
+    roll_data.appendleft(new_roll_data*RAD_TO_DEG_FACTOR)
     datatoplot = roll_data.pop() # only want 100 values to plot at any one time
-    pitch_data.appendleft(new_pitch_data)
+    pitch_data.appendleft(new_pitch_data*RAD_TO_DEG_FACTOR)
     datatoplot = pitch_data.pop() # only want 100 values to plot at any one time    
-    yaw_data.appendleft(new_yaw_data)
+    yaw_data.appendleft(new_yaw_data*RAD_TO_DEG_FACTOR)
     datatoplot = yaw_data.pop() # only want 100 values to plot at any one time    
     
     sema.notify() # notify plot that data is available
@@ -108,21 +111,26 @@ def comReader(sample_nbr):
 app = QtGui.QApplication([])
 
 win = pg.GraphicsWindow(title="Basic plotting examples")
-win.resize(1000,600)
+#win.resize(1000,600)
 win.setWindowTitle('pyqtgraph example: Plotting')
 pg.setConfigOptions(antialias=True) # Enable antialiasing for prettier plots
 
-roll_plot = win.addPlot(title="Real-time ish roll angle[rad] plot")
-roll_plot_curve = roll_plot.plot(pen='r', name='Roll[rad]')
+roll_plot = win.addPlot(title="Roll angle [deg]")
+roll_plot_curve = roll_plot.plot(pen='r', name='Roll [rad]')
 roll_plot.showGrid(x=True, y=True)
+roll_plot.setYRange(-180, 180)
+
 win.nextRow()
-pitch_plot = win.addPlot(title="Real-time ish pitch angle[rad] plot")
-pitch_plot_curve = pitch_plot.plot(pen='g', name='Pitch[rad]')
+pitch_plot = win.addPlot(title="Pitch angle [deg]")
+pitch_plot_curve = pitch_plot.plot(pen='g', name='Pitch [rad]')
 pitch_plot.showGrid(x=True, y=True)
+pitch_plot.setYRange(-180, 180)
+
 win.nextRow()
-yaw_plot = win.addPlot(title="Real-time ish yaw angle[rad] plot")
-yaw_plot_curve = yaw_plot.plot(pen='y', name='Yaw[rad]')
+yaw_plot = win.addPlot(title="Yaw angle [deg]")
+yaw_plot_curve = yaw_plot.plot(pen='y', name='Yaw [deg]')
 yaw_plot.showGrid(x=True, y=True)
+yaw_plot.setYRange(-180, 180)
 
 def plotUpdate():
     global ptr, roll_plot, pitch_plot, yaw_plot
@@ -138,14 +146,14 @@ timer.start(25)
 interval_s = cli_args.interval_ms/float(1000)
 sample_nbr = int(float(cli_args.duration_s / interval_s))
 
-fcb_serial = serial.Serial( "\\.\COM%d" % (cli_args.com), 115200, parity=serial.PARITY_NONE, rtscts=False, xonxoff=False, timeout=1, write_timeout=1)
+fcb_serial = serial.Serial(cli_args.com, 115200, parity=serial.PARITY_NONE, rtscts=False, xonxoff=False, timeout=1, write_timeout=1)
 myComReaderThread = threading.Thread(target=comReader, name="tComReader", args=(sample_nbr,))
 myComReaderThread.daemon = True
 myComReaderThread.start()
-fcb_serial.write("about\n") # data does not show up when removing this line ... work-around
+fcb_serial.write("about\r") # data does not show up when removing this line ... work-around
 time.sleep(1)
 try:
-    fcb_serial.write("start-state-sampling %d %d p" % (cli_args.interval_ms, cli_args.duration_s))
+    fcb_serial.write("start-state-sampling %d %d p" % (cli_args.interval_ms, cli_args.duration_s) + "\r")
 except serial.SerialTimeoutException as ste:
     fcb_serial.close()
     pprint(ste.__str__())
